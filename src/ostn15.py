72c6e3d8c662a16e6b7f26faa38d436a8275ac65
Stephan Hügel <shugel@tcd.ie>
2018-12-13 14:38:00 +0000

    Formatting
    [ci skip]

--- src/ostn15.py
+++ src/ostn15.py
@@ -5,24 +5,21 @@ from ctypes import c_int32, c_double, Structure
 
 
 class GridRefs(Structure):
-    _fields_ = [("eastings", c_int32),
-                ("northings", c_int32)]
+    _fields_ = [("eastings", c_int32), ("northings", c_int32)]
 
     def __str__(self):
         return "({}, {})".format(self.eastings, self.northings)
 
 
 class Shifts(Structure):
-    _fields_ = [("x_shift", c_double),
-                ("y_shift", c_double),
-                ("z_shift", c_double)]
+    _fields_ = [("x_shift", c_double), ("y_shift", c_double), ("z_shift", c_double)]
 
     def __str__(self):
         return "({}, {}, {})".format(self.x_shift, self.y_shift, self.z_shift)
 
 
-prefix = {"win32": ''}.get(sys.platform, 'lib')
-extension = {'darwin': '.dylib', 'win32': '.dll'}.get(sys.platform, '.so')
+prefix = {"win32": ""}.get(sys.platform, "lib")
+extension = {"darwin": ".dylib", "win32": ".dll"}.get(sys.platform, ".so")
 lib = ctypes.cdll.LoadLibrary("target/release/" + prefix + "ostn15_phf" + extension)
 lib.get_shifts_ffi.argtypes = (GridRefs,)
 lib.get_shifts_ffi.restype = Shifts
