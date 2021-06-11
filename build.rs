f6952c04546a746b06125f9dbca9e68f7aa0f1bc
Stephan Hügel <shugel@tcd.ie>
2021-06-11 11:24:03 +0100

    Better way of conditionally running tests

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 use phf_codegen;
 use rusqlite::Connection;
 
+use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufWriter;
@@ -15,41 +16,45 @@ struct Shift {
     height: f64,
 }
 
+// run build script like BUILD_ENABLED=1 cargo build
 fn main() {
-    let conn = Connection::open("src/OSTN15.db").unwrap();
+    let build_enabled = env::var("BUILD_ENABLED").map(|v| v == "1").unwrap_or(false); // don't run by default
+    if build_enabled {
+        let conn = Connection::open("src/OSTN15.db").unwrap();
 
-    let mut outfile = BufWriter::new(File::create(GENERATED_FILE).unwrap());
-    write!(outfile, "static OSTN15: phf::Map<i32, (f64, f64, f64)> = ").unwrap();
-    let mut stmt = conn.prepare("SELECT * FROM ostn15").unwrap();
-    let ostn15_iter = stmt
-        .query_map([], |row| {
-            Ok(Shift {
-                key: row.get(0).unwrap(),
-                eastings: row.get(1).unwrap(),
-                northings: row.get(2).unwrap(),
-                height: row.get(3).unwrap(),
+        let mut outfile = BufWriter::new(File::create(GENERATED_FILE).unwrap());
+        write!(outfile, "static OSTN15: phf::Map<i32, (f64, f64, f64)> = ").unwrap();
+        let mut stmt = conn.prepare("SELECT * FROM ostn15").unwrap();
+        let ostn15_iter = stmt
+            .query_map([], |row| {
+                Ok(Shift {
+                    key: row.get(0).unwrap(),
+                    eastings: row.get(1).unwrap(),
+                    northings: row.get(2).unwrap(),
+                    height: row.get(3).unwrap(),
+                })
             })
-        })
-        .unwrap();
-    let mut keys = vec![];
-    let mut values = vec![];
-    for each in ostn15_iter {
-        let record = each.unwrap();
-        keys.push(record.key);
-        values.push((
-            record.eastings as f64,
-            record.northings as f64,
-            record.height as f64,
-        ));
+            .unwrap();
+        let mut keys = vec![];
+        let mut values = vec![];
+        for each in ostn15_iter {
+            let record = each.unwrap();
+            keys.push(record.key);
+            values.push((
+                record.eastings as f64,
+                record.northings as f64,
+                record.height as f64,
+            ));
+        }
+        let results: Vec<_> = keys.iter().zip(values.iter()).collect();
+        let mut map = phf_codegen::Map::<i32>::new();
+        for &(ref key, val) in &results {
+            map.entry(
+                **key,
+                &format!("({:.3}, {:.3}, {:.3})", val.0, val.1, val.2),
+            );
+        }
+        writeln!(&mut outfile, "{}", map.build()).unwrap();
+        writeln!(&mut outfile, ";").unwrap();
     }
-    let results: Vec<_> = keys.iter().zip(values.iter()).collect();
-    let mut map = phf_codegen::Map::<i32>::new();
-    for &(ref key, val) in &results {
-        map.entry(
-            **key,
-            &format!("({:.3}, {:.3}, {:.3})", val.0, val.1, val.2),
-        );
-    }
-    writeln!(&mut outfile, "{}", map.build()).unwrap();
-    writeln!(&mut outfile, ";").unwrap();
 }
