f5468b65969c19fdf09e416f0d8f773ee30e66ba
Stephan Hügel <shugel@tcd.ie>
2023-03-24 17:12:02 +0000

    Try to disable build script and its deps on linux.aarch64

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
-use std::env;
 use phf_codegen;
+use std::env;
 const GENERATED_FILE: &'static str = "src/ostn15.rs";
 
 #[derive(Debug)]
@@ -14,45 +14,54 @@ struct Shift {
 fn main() {
     let build_enabled = env::var("BUILD_ENABLED").map(|v| v == "1").unwrap_or(false); // don't run by default
     if build_enabled {
-        use rusqlite::Connection;
-        use std::fs::File;
-        use std::io::prelude::*;
-        use std::io::BufWriter;
-        let conn = Connection::open("src/OSTN15.db").unwrap();
+        #[cfg(target_arch = "aarch64-unknown-linux-gnu")]
+        {
+            eprintln!("aarch64-unknown-linux-gnu isn't compatible with the rusqlite dependency in this build script. It will not be run");
+        }
+        // this build section will not run run on Linux aarch64
+        #[cfg(not(target_arch = "aarch64-unknown-linux-gnu"))]
+        {
+            eprintln!("Compatible architecture found for build script. Proceeding.");
+            use rusqlite::Connection;
+            use std::fs::File;
+            use std::io::prelude::*;
+            use std::io::BufWriter;
+            let conn = Connection::open("src/OSTN15.db").unwrap();
 
-        let mut outfile = BufWriter::new(File::create(GENERATED_FILE).unwrap());
-        write!(outfile, "static OSTN15: phf::Map<i32, (f64, f64, f64)> = ").unwrap();
-        let mut stmt = conn.prepare("SELECT * FROM ostn15").unwrap();
-        let ostn15_iter = stmt
-            .query_map([], |row| {
-                Ok(Shift {
-                    key: row.get(0).unwrap(),
-                    eastings: row.get(1).unwrap(),
-                    northings: row.get(2).unwrap(),
-                    height: row.get(3).unwrap(),
+            let mut outfile = BufWriter::new(File::create(GENERATED_FILE).unwrap());
+            write!(outfile, "static OSTN15: phf::Map<i32, (f64, f64, f64)> = ").unwrap();
+            let mut stmt = conn.prepare("SELECT * FROM ostn15").unwrap();
+            let ostn15_iter = stmt
+                .query_map([], |row| {
+                    Ok(Shift {
+                        key: row.get(0).unwrap(),
+                        eastings: row.get(1).unwrap(),
+                        northings: row.get(2).unwrap(),
+                        height: row.get(3).unwrap(),
+                    })
                 })
-            })
-            .unwrap();
-        let mut keys = vec![];
-        let mut values = vec![];
-        for each in ostn15_iter {
-            let record = each.unwrap();
-            keys.push(record.key);
-            values.push((
-                record.eastings as f64,
-                record.northings as f64,
-                record.height as f64,
-            ));
+                .unwrap();
+            let mut keys = vec![];
+            let mut values = vec![];
+            for each in ostn15_iter {
+                let record = each.unwrap();
+                keys.push(record.key);
+                values.push((
+                    record.eastings as f64,
+                    record.northings as f64,
+                    record.height as f64,
+                ));
+            }
+            let results: Vec<_> = keys.iter().zip(values.iter()).collect();
+            let mut map = phf_codegen::Map::<i32>::new();
+            for &(ref key, val) in &results {
+                map.entry(
+                    **key,
+                    &format!("({:.3}, {:.3}, {:.3})", val.0, val.1, val.2),
+                );
+            }
+            writeln!(&mut outfile, "{}", map.build()).unwrap();
+            writeln!(&mut outfile, ";").unwrap();
         }
-        let results: Vec<_> = keys.iter().zip(values.iter()).collect();
-        let mut map = phf_codegen::Map::<i32>::new();
-        for &(ref key, val) in &results {
-            map.entry(
-                **key,
-                &format!("({:.3}, {:.3}, {:.3})", val.0, val.1, val.2),
-            );
-        }
-        writeln!(&mut outfile, "{}", map.build()).unwrap();
-        writeln!(&mut outfile, ";").unwrap();
     }
 }
